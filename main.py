1198b1ab87acf420181894b600877371c8dbef7a
Oscar Pedreño <pedreno.oscar@outlook.com>
2023-01-17 16:29:05 +0100

    add superclass player and subclasses player_1 & player_2, create movement features.

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from turtle import Turtle, Screen
+from player import Player_1, Player_2
+import time
+
+GAME_IS_ON = True
+
 
 screen = Screen()
 screen.setup(width=1200, height = 800)
 screen.bgcolor("black")
+screen.title("PONG GAME")
+screen.tracer(0)
+
+player_1 = Player_1()
+player_2 = Player_2()
+
+
+screen.listen()
+screen.onkey(key= "Up", fun=player_1.move_up)
+screen.onkey(key= "Down", fun=player_1.move_down)
+screen.onkey(key="w", fun=player_2.move_up)
+screen.onkey(key="s", fun=player_2.move_down)
+
+while GAME_IS_ON:
+    screen.update()
+    time.sleep(0.08)
+
+
+
+
+
+
 
 
 
---
+++ player.py
@@ -0,0 +1,61 @@
+from turtle import Turtle
+
+PLAYER_INITIAL_POSITION = [(0, 50), (0, 30), (0, 10), (0, -10), (0, -30)]
+PLAYER_1_XCOR = -550
+PLAYER_2_XCOR = 550
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.player = []
+        self.create_new_player()
+        self.upside = self.player[0]
+        self.length = len(self.player)
+        self.downside = self.player[-1]
+        print(self.player)
+
+    def create_new_player(self):
+        for position in PLAYER_INITIAL_POSITION:
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.up()
+            new_segment.setheading(90)
+            new_segment.goto(position)
+            self.player.append(new_segment)
+
+    def move_up(self):
+        for seg in range(self.length-1, 0, -1):
+            new_position = self.player[seg-1].position()
+            self.player[seg].goto(new_position)
+        self.upside.forward(15)
+        
+    def move_down(self):
+        for seg in range(0, self.length-1):
+            new_position = self.player[seg+1].position()
+            self.player[seg].goto(new_position)
+        self.downside.setheading(270)
+        self.downside.forward(15)
+
+class Player_1(Player):
+    def __init__(self):
+        super().__init__()
+        self.set_player_1()
+
+    def set_player_1(self):
+        for seg in self.player:
+            seg.setx(-550)
+
+class Player_2(Player):
+    def __init__(self):
+        super().__init__()
+        self.set_player_2()
+
+    def set_player_2(self):
+        for seg in self.player:
+            seg.setx(550)
+        
+            
+
+
+    
